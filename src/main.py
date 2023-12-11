9f3d84d591ac541e16f33502bcbfd8014fb3cf6b
Joseph Rance <josephhenryrance@gmail.com>
2023-12-11 22:38:06 +0000

    add other eval function

--- src/main.py
+++ src/main.py
@@ -180,6 +180,50 @@ class ClassSubsetDataset(Dataset):
     def __getitem__(self, idx):
         return self.dataset[self.indexes[idx]]
 
+def get_evaluate_fn(model, loaders, file_name="", device="cuda"):
+
+    model = model().to(device)
+
+    def evaluate(training_round, parameters, config):
+
+        nonlocal model, device, file_name
+
+        keys = [k for k in model.state_dict().keys() if 'num_batches_tracked' not in k]
+        params_dict = zip(keys, parameters)
+        state_dict = OrderedDict({k: torch.Tensor(v) for k, v in params_dict})
+        model.load_state_dict(state_dict, strict=True)
+
+        model.eval()
+
+        with torch.no_grad():
+
+            overall_loss = None
+            metrics = {}
+
+            for (name, loader) in loaders:
+
+                loss = total = correct = 0
+                for x, y in loader:
+                    x, y = x.to(device), y.to(device)
+
+                    z = model(x)
+                    loss += F.cross_entropy(z, y)
+
+                    total += y.size(0)
+                    correct += (torch.max(z.data, 1)[1] == y).sum().item()
+
+                metrics[f"loss_{name}"] = loss.item()
+                metrics[f"accuracy_{name}"] = correct / total
+
+                if name == "all":
+                    overall_loss = loss / len(loader)
+
+        np.save(f"outputs/metrics_{training_round}_{file_name}.npy", np.array([metrics], dtype=object), allow_pickle=True)
+
+        return overall_loss, metrics
+
+    return evaluate
+
 if __name__ == "__main__":
 
 
@@ -243,7 +287,7 @@ if __name__ == "__main__":
         initial_parameters=fl.common.ndarrays_to_parameters([
             val.numpy() for n, val in ResNet50().state_dict().items() if 'num_batches_tracked' not in n
         ]),
-        evaluate_fn=get_evaluate_fn(ResNet50, {}, test_loaders, config),
+        evaluate_fn=get_evaluate_fn(ResNet50, test_loaders, file_name=str(num_clients)),
         fraction_fit=1,
         on_fit_config_fn=lambda x : {"round": x}
     )
