af059d9173ede18bd02737001183b458e79e7cec
Joseph Rance <josephhenryrance@gmail.com>
2023-12-13 17:18:36 +0000

    update slurm

--- scripts/testing.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cat configs/templates/cifar10.yaml <(echo) configs/templates/baseline.yaml > configs/gen_config.yaml
-python src/main.py configs/gen_config.yaml -c $1 -g $2
---
+++ scripts/testing_cifar10.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cat configs/templates/cifar10.yaml <(echo) configs/templates/baseline.yaml > configs/gen_config.yaml
+python src/main.py configs/gen_config.yaml -c $1 -g $2
+
+# srun -c 16 --gres=gpu:2 -w ngongotaha bash scripts/slurm.sh scripts/testing_cifar10.sh 16 2
