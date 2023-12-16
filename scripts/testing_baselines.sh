8c269c9f5d9bc92555511e02dae9307d1cbec460
Joseph Rance <josephhenryrance@gmail.com>
2023-12-16 21:17:46 +0000

    add baseline script

---
+++ scripts/testing_baselines.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "getting adult dataset"
+bash scripts/get_adult.sh > outputs/download
+for DATASET in cifar10 adult
+do
+    for SEED in 0 1 2
+    do
+        echo "running baseline on $DATASET"
+        cat configs/templates/$DATASET.yaml <(echo) configs/templates/baseline.yaml > configs/gen_config.yaml
+        sed -i -e "s/\seed: 0/seed: $SEED/g" configs/gen_config.yaml
+        python src/main.py configs/gen_config.yaml -c $1 -g $2
+    done
+done
