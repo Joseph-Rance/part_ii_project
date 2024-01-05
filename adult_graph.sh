96592d3e742761d56d2ddea3d81d6b4ee653eac2
Joseph Rance <josephhenryrance@gmail.com>
2024-01-05 22:33:12 +0000

    add temp scripts

---
+++ adult_graph.sh
@@ -0,0 +1,13 @@
+sed -i -e "s/checkpoint_period: 40/checkpoint_period: 1/" configs/templates/adult.yaml
+
+srun -c 16 --gres=gpu:2 -w ngongotaha bash scripts/slurm.sh scripts/adult/no_def/unfair.sh 16 2
+srun -c 16 --gres=gpu:2 -w ngongotaha bash scripts/slurm.sh scripts/adult/no_def/backdoor.sh 16 2
+srun -c 16 --gres=gpu:2 -w ngongotaha bash scripts/slurm.sh scripts/adult/no_def/baseline.sh 16 2
+
+sed -i -e "s/start_round: 0/start_round: 20/" configs/templates/fairness_attack.yaml
+
+srun -c 16 --gres=gpu:2 -w ngongotaha bash scripts/slurm.sh scripts/adult/no_def/unfair.sh 16 2
+srun -c 16 --gres=gpu:2 -w ngongotaha bash scripts/slurm.sh scripts/adult/no_def/backdoor.sh 16 2
+srun -c 16 --gres=gpu:2 -w ngongotaha bash scripts/slurm.sh scripts/adult/no_def/baseline.sh 16 2
+
+zip -r graph_outs.zip outputs.zip
