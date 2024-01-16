f770b2fd03f88c920a8ca9d9d7f344edcbe35485
Joseph Rance <josephhenryrance@gmail.com>
2024-01-16 22:59:31 +0000

    fix paths

--- krum_testing.sh
+++ krum_testing.sh
@@ -1,20 +1,20 @@
-sed -i -e "s/num: 10/num: 10/" configs/krum.yaml
+sed -i -e "s/num: 10/num: 10/" configs/templates/adult.yaml
 
-sed -i -e "s/f: 1/f: 1/" configs/gen_config.yaml
+sed -i -e "s/f: 1/f: 1/" configs/templates/krum.yaml
 srun -c 16 --gres=gpu:2 -w ngongotaha bash scripts/slurm.sh scripts/adult/krum/unfair.sh 16 2
-sed -i -e "s/f: 1/f: 2/" configs/gen_config.yaml
+sed -i -e "s/f: 1/f: 2/" configs/templates/krum.yaml
 srun -c 16 --gres=gpu:2 -w ngongotaha bash scripts/slurm.sh scripts/adult/krum/unfair.sh 16 2
-sed -i -e "s/f: 1/f: 3/" configs/gen_config.yaml
+sed -i -e "s/f: 1/f: 3/" configs/templates/krum.yaml
 srun -c 16 --gres=gpu:2 -w ngongotaha bash scripts/slurm.sh scripts/adult/krum/unfair.sh 16 2
-sed -i -e "s/f: 1/f: 4/" configs/gen_config.yaml
+sed -i -e "s/f: 1/f: 4/" configs/templates/krum.yaml
 srun -c 16 --gres=gpu:2 -w ngongotaha bash scripts/slurm.sh scripts/adult/krum/unfair.sh 16 2
-sed -i -e "s/f: 1/f: 5/" configs/gen_config.yaml
+sed -i -e "s/f: 1/f: 5/" configs/templates/krum.yaml
 srun -c 16 --gres=gpu:2 -w ngongotaha bash scripts/slurm.sh scripts/adult/krum/unfair.sh 16 2
-sed -i -e "s/f: 1/f: 6/" configs/gen_config.yaml
+sed -i -e "s/f: 1/f: 6/" configs/templates/krum.yaml
 srun -c 16 --gres=gpu:2 -w ngongotaha bash scripts/slurm.sh scripts/adult/krum/unfair.sh 16 2
-sed -i -e "s/f: 1/f: 7/" configs/gen_config.yaml
+sed -i -e "s/f: 1/f: 7/" configs/templates/krum.yaml
 srun -c 16 --gres=gpu:2 -w ngongotaha bash scripts/slurm.sh scripts/adult/krum/unfair.sh 16 2
-sed -i -e "s/f: 1/f: 8/" configs/gen_config.yaml
+sed -i -e "s/f: 1/f: 8/" configs/templates/krum.yaml
 srun -c 16 --gres=gpu:2 -w ngongotaha bash scripts/slurm.sh scripts/adult/krum/unfair.sh 16 2
-sed -i -e "s/f: 1/f: 9/" configs/gen_config.yaml
+sed -i -e "s/f: 1/f: 9/" configs/templates/krum.yaml
 srun -c 16 --gres=gpu:2 -w ngongotaha bash scripts/slurm.sh scripts/adult/krum/unfair.sh 16 2
