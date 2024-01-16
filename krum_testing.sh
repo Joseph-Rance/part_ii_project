4d4f91cb4311a68901657136d7b91dd9df49dc62
Joseph Rance <josephhenryrance@gmail.com>
2024-01-16 23:09:13 +0000

    add more krum testing

--- krum_testing.sh
+++ krum_testing.sh
@@ -1,20 +1,19 @@
 sed -i -e "s/num: 10/num: 10/" configs/templates/adult.yaml
 
-sed -i -e "s/f: 1/f: 1/" configs/templates/krum.yaml
 srun -c 16 --gres=gpu:2 -w ngongotaha bash scripts/slurm.sh scripts/adult/krum/unfair.sh 16 2
 sed -i -e "s/f: 1/f: 2/" configs/templates/krum.yaml
 srun -c 16 --gres=gpu:2 -w ngongotaha bash scripts/slurm.sh scripts/adult/krum/unfair.sh 16 2
-sed -i -e "s/f: 1/f: 3/" configs/templates/krum.yaml
+sed -i -e "s/f: 2/f: 3/" configs/templates/krum.yaml
 srun -c 16 --gres=gpu:2 -w ngongotaha bash scripts/slurm.sh scripts/adult/krum/unfair.sh 16 2
-sed -i -e "s/f: 1/f: 4/" configs/templates/krum.yaml
+sed -i -e "s/f: 3/f: 4/" configs/templates/krum.yaml
 srun -c 16 --gres=gpu:2 -w ngongotaha bash scripts/slurm.sh scripts/adult/krum/unfair.sh 16 2
-sed -i -e "s/f: 1/f: 5/" configs/templates/krum.yaml
+sed -i -e "s/f: 4/f: 5/" configs/templates/krum.yaml
 srun -c 16 --gres=gpu:2 -w ngongotaha bash scripts/slurm.sh scripts/adult/krum/unfair.sh 16 2
-sed -i -e "s/f: 1/f: 6/" configs/templates/krum.yaml
+sed -i -e "s/f: 5/f: 6/" configs/templates/krum.yaml
 srun -c 16 --gres=gpu:2 -w ngongotaha bash scripts/slurm.sh scripts/adult/krum/unfair.sh 16 2
-sed -i -e "s/f: 1/f: 7/" configs/templates/krum.yaml
+sed -i -e "s/f: 6/f: 7/" configs/templates/krum.yaml
 srun -c 16 --gres=gpu:2 -w ngongotaha bash scripts/slurm.sh scripts/adult/krum/unfair.sh 16 2
-sed -i -e "s/f: 1/f: 8/" configs/templates/krum.yaml
+sed -i -e "s/f: 7/f: 8/" configs/templates/krum.yaml
 srun -c 16 --gres=gpu:2 -w ngongotaha bash scripts/slurm.sh scripts/adult/krum/unfair.sh 16 2
-sed -i -e "s/f: 1/f: 9/" configs/templates/krum.yaml
+sed -i -e "s/f: 8/f: 9/" configs/templates/krum.yaml
 srun -c 16 --gres=gpu:2 -w ngongotaha bash scripts/slurm.sh scripts/adult/krum/unfair.sh 16 2
