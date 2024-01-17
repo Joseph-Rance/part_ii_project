c02ed8ceb95cf6db3fadfd974e5902a946d10e2d
Joseph Rance <josephhenryrance@gmail.com>
2024-01-17 00:53:18 +0000

    correctly handle multiline substitutions

--- scripts/adult/trim_mean/backdoor.sh
+++ scripts/adult/trim_mean/backdoor.sh
@@ -2,7 +2,7 @@
 bash scripts/get_adult.sh > outputs/download
 cat configs/templates/adult.yaml <(echo) configs/templates/backdoor_attack.yaml \
                                  <(echo) configs/templates/trimmed_mean.yaml > configs/gen_config.yaml
-sed -i -e "s/backdoor_attack\n    start_round: 0/backdoor_attack\n    start_round: 30/" configs/gen_config.yaml
+sed -z -i -e "s/backdoor_attack\n    start_round: 0/backdoor_attack\n    start_round: 30/" configs/gen_config.yaml
 python src/main.py configs/gen_config.yaml -c $1 -g $2
 
 # srun -c 16 --gres=gpu:2 -w ngongotaha bash scripts/slurm.sh scripts/adult/trim_mean/backdoor.sh 16 2
